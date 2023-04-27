06d2efcbe29f39b40a5cf4b18889fc6b9967ffe2
rondido <rppr001@naver.com>
2023-04-28 01:57:58 +0900

    problem 4번 commit

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,50 @@
-function problem4(word) {
-  var answer;
+function atozSentenceArr() {
+  const atozArr = [];
+  for (let i = 97; i <= 122; i++) {
+    atozArr.push(String.fromCharCode(i));
+  }
+  return atozArr;
+}
+
+function ztoaSentenceArr() {
+  const ztoaArr = [];
+  for (let i = 122; i >= 97; i--) {
+    ztoaArr.push(String.fromCharCode(i));
+  }
+  return ztoaArr;
+}
+
+function getReverseSentence(word, atoz, ztoa) {
+  let answer = "";
+  const wordArr = word.split("");
+  wordArr.map((value) => {
+    let reverseSentence = value;
+    if (atoz.includes(value.toLowerCase())) {
+      let index = atoz.indexOf(value.toLowerCase());
+      if (value === value.toLowerCase()) {
+        reverseSentence = ztoa[index];
+      } else {
+        reverseSentence = ztoa[index].toUpperCase();
+      }
+    }
+    if (ztoa.includes(value.toLowerCase())) {
+      let index = ztoa.indexOf(value.toLowerCase());
+      if (value === value.toLowerCase()) {
+        reverseSentence = atoz[index];
+      } else {
+        reverseSentence = atoz[index].toUpperCase();
+      }
+    }
+    answer += reverseSentence;
+  });
   return answer;
+}
+
+function problem4(word) {
+  let atoz = atozSentenceArr();
+  let ztoa = ztoaSentenceArr();
+  const result = getReverseSentence(word, atoz, ztoa);
+  return result;
 }
 
 module.exports = problem4;
