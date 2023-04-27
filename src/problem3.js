06432b6bd821c40558228bd5e1c80384f45b63bf
rondido <rppr001@naver.com>
2023-04-27 22:48:29 +0900

    feat problem 3번

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
-function problem3(number) {
-  var answer;
+function arrValueCheck(arr) {
+  answer = arr.filter(
+    (num) => num === "3" || num === "6" || num === "9"
+  ).length;
   return answer;
+}
+function createArr(number) {
+  let arr = Array.from({ length: number }, (v, i) => i + 1)
+    .toString()
+    .split("");
+  return arr;
+}
+// 3  6  9 13 16 19 23 26 29 33 36 39
+function problem3(number) {
+  const arr = createArr(number);
+  return arrValueCheck(arr);
 }
 
 module.exports = problem3;
