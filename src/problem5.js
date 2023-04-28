c7e3991c2cdde98deb297ad075ccc7751d6d9d58
rondido <rppr001@naver.com>
2023-04-28 17:00:55 +0900

    problem5 commit

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
-function problem5(money) {
-  var answer;
+function greedyMoney(money) {
+  var answer = [];
+  const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let coin of moneyList) {
+    answer.push(Math.floor(money / coin)); // 1
+    money -= coin * Math.floor(money / coin); // 50000 * 2 100000
+  }
   return answer;
 }
 
+function problem5(money) {
+  return greedyMoney(money);
+}
 module.exports = problem5;
